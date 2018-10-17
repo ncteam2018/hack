d7a12fff38736f49cf4083aaa863bde6b13818db
juliaPesina <pesina_ja@bk.ru>
2018-10-17 12:37:08 +0300

    Initial commit

---
+++ src/main/java/com/netcracker/hack/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.netcracker.hack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MessageController {
+    @GetMapping
+    public String list(){
+        return "SearchHack!";
+    }
+}
