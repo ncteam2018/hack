d1030bca37c9597eb3b23fc65b64070c4edd9f4c
Dmitry <kuklindima@yandex.ru>
2018-10-22 18:13:47 +0300

    change swagger configuration

--- src/main/java/com/netcracker/hack/rest/swagger/SwaggerConfig.java
+++ src/main/java/com/netcracker/hack/rest/swagger/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 					.select()
 						.apis(RequestHandlerSelectors.basePackage("com.netcracker.hack.rest"))
-						.paths(regex("/people.*"))
+						.paths(regex("/api.*"))
 						.build();
 		
 	}
