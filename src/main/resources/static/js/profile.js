77182e5bc6d68c1c5c0d71eb3ab1c5fb1db1400f
Dmitry <kuklindima@yandex.ru>
2018-10-31 21:48:08 +0300

    Fix script url

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,6 +1,6 @@
 function loadUserProfile() {
    
-	fetch('api/profile/28a82e3c-b777-4a77-b997-46b6f541c95e')
+	fetch('/api/profile/login/test')
 	  .then(function(response) {
 			
 	   return response.json();
