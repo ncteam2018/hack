568297888c789a463abd30831ca7b1fd5535ea7f
juliaPesina <pesina_ja@bk.ru>
2018-11-07 18:20:45 +0300

    Refactoring

--- src/main/java/com/netcracker/hack/controller/web/ProfileWebController.java
+++ src/main/java/com/netcracker/hack/controller/web/ProfileWebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ProfileController {
+public class ProfileWebController {
 
   @RequestMapping(path = "/profile", method = RequestMethod.GET)
   public String getProfilePage(String loginError) {
