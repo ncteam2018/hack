07bc1dedf7cc9d0838bc259a1e8603a54c9608b7
juliaPesina <pesina_ja@bk.ru>
2018-12-05 17:27:27 +0300

    Events: Type added as a title of event

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -16,31 +16,48 @@ $(document).ready(function() {
 $("#events").addClass('text-warning');
 eventList = new Array();
 function loadEventInformation() {
-    fetch("api/event?eventType=1&ownerID=7eaf7c29-8914-4038-b6cf-7530a42bbead", {
-                method : 'GET',
-                headers : getDefaultHeaders(),
-                credentials : "same-origin"
-            }).then(function(response) {
-                return response.json();
-            }).then(function (value) {
-                for (var i = 0; i < value.length; i++) {
-                    for (var j = 0; j < value[i].length; j++) {
-                        eventList.push(value[i][j]);
-                    }
+
+
+    fetch("/api/profile/me", {
+        method : 'GET',
+        headers : getDefaultHeaders(),
+        credentials : "same-origin"
+    }).then(function(response) {
+        return response.json();
+    }).then(function(myProfile) {
+        UserID = myProfile.uuid;
+        let query = "api/event?eventType=1&ownerID=" + UserID;
+        fetch(query, {
+            method : 'GET',
+            headers : getDefaultHeaders(),
+            credentials : "same-origin"
+        }).then(function(response) {
+            return response.json();
+        }).then(function (value) {
+            for (var i = 0; i < value.length; i++) {
+                for (var j = 0; j < value[i].length; j++) {
+                    eventList.push(value[i][j]);
                 }
-        $("#eventList").html("");
-        eventList.forEach(function(eventItem, index, arr) {
-            eventItem.index = index;
-            $("#eventItem").tmpl(eventItem).appendTo("#eventList");
-        });
-                // $("#message").html(value[0][0].message);
-                // $("#dateOfCreation").html(value[0][0].dateOfCreation);
-                //
-                // $("#id").html(value[0][0].id);
+            }
+            $("#eventList").html("");
+            eventList.forEach(function(eventItem, index, arr) {
+                eventItem.index = index;
+                $("#eventItem").tmpl(eventItem).appendTo("#eventList");
+            });
+            // $("#message").html(value[0][0].message);
+            // $("#dateOfCreation").html(value[0][0].dateOfCreation);
+            //
+            // $("#id").html(value[0][0].id);
 
         })
-    // $('#loadingIcon').remove();
-    $('#userProfile').css('visibility', 'visible');
+        // $('#loadingIcon').remove();
+        $('#userProfile').css('visibility', 'visible');
+
+
+
+
+    });
+
 }
 
 
@@ -48,9 +65,8 @@ function loadEventInformation() {
 function changeState(eventId, state){
     var q = "api/event/" + eventId;
     fetch(q, {
-            method : 'PUT',
-            headers : getDefaultHeaders(),
-            body: JSON.stringify(state)
-        })
+        method : 'PUT',
+        headers : getDefaultHeaders(),
+        body: JSON.stringify(state)
+    })
 }
-
