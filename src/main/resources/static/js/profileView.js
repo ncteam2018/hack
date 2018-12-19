1deae0eb1ece01a6d2addaa14dd97e9e886175f8
Dmitry <kuklindima@yandex.ru>
2018-12-19 03:28:50 +0300

    Add hack and profile pages.
    Frontend update.

---
+++ src/main/resources/static/js/profileView.js
@@ -0,0 +1,46 @@
+function getDefaultHeaders() {
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	return headers;
+}
+
+function loadUserProfile() {
+
+	let query = "/api/profile/" + $("#userUUID").html();
+	fetch(query, {
+		method : 'GET',
+		headers : getDefaultHeaders(),
+		credentials : "same-origin"
+	}).then(function(response) {
+		return response.json();
+
+	}).then(
+			function(profile) {
+
+				$("#nickname").html(profile.login);
+				$("#fio").html(
+						profile.lastName + " " + profile.firstName + " "
+								+ profile.middleName);
+				$("#gender").html(profile.gender);
+				$("#birth").html(profile.dateOfBirth);
+				$("#place").html(profile.city);
+				$("#email").html(profile.email);
+				$("#phone").html(profile.phone);
+				$("#skype").html(profile.skype);
+				$("#about").html(profile.about);
+				$("#institution").html(profile.institution);
+				$("#faculty").html(profile.faculty);
+				$("#course").html(profile.course);
+				$("#placeOfWork").html(profile.placeOfWork);
+				$("#position").html(profile.position);
+
+				if (profile.companyData != null) {
+					$("#companyData").html(profile.companyData.companyName);
+					$("#aboutCompany").html(profile.companyData.about);
+				}
+
+				$('#loadingIcon').remove();
+				$('#userProfile').css('display', 'block');
+			});
+
+}
