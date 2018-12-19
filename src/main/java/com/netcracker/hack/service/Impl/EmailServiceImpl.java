8757b4f955a4d5c126cfa9161f5ecbef36c6fb1d
Dmitry <kuklindima@yandex.ru>
2018-12-19 17:06:49 +0300

    fix

--- src/main/java/com/netcracker/hack/service/Impl/EmailServiceImpl.java
+++ src/main/java/com/netcracker/hack/service/Impl/EmailServiceImpl.java
@@ -5,10 +5,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-@Service
 public class EmailServiceImpl {
   
-  @Autowired
+ 
   private JavaMailSender emailSender;
 
   public void sendSimpleMessage(String to, String subject, String text) {
